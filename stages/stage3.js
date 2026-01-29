7a1e00dce1e75129544c6007efec739f591f0c15
kokokota <76462208+koyata-matsu@users.noreply.github.com>
2026-01-29 17:38:43 +0900

    Update stage3 corrected HCO3 choices

--- stages/stage3.js
+++ stages/stage3.js
@@ -90,12 +90,19 @@ function makeQ_type3_corrHCO3(){
   const corrAg = to1(randInt(12,35));     // 補正AGを与える
   const hco3 = to1(randInt(4,28));        // HCO3を与える
   const corrHco3 = to1(calcCorrHCO3(hco3, corrAg));
-
-  const {options, correctIndex} = makeOptions(corrHco3);
+  let correctLabel = "純粋なAG開大型";
+  if(corrHco3 > 26) correctLabel = "代謝性アルカローシス合併";
+  if(corrHco3 < 22) correctLabel = "非開大型代謝性アシドーシス合併";
+  const options = shuffle([
+    "代謝性アルカローシス合併",
+    "非開大型代謝性アシドーシス合併",
+    "純粋なAG開大型",
+  ]);
+  const correctIndex = options.indexOf(correctLabel);
 
   return {
-    kind: "calc",
-    prompt: "補正HCO3は？",
+    kind: "judge",
+    prompt: "AG開大性代謝性アシドーシスが見られた。補正HCO3の値は？ AG。。。。。",
     items: [
       { k:"補正AG", v: corrAg.toFixed(1) },
       { k:"HCO3", v: hco3.toFixed(1) },
@@ -150,6 +157,7 @@ export function createStage3(){
 
     // 毎問choicesが違う
     getChoices(q){
+      if(q.kind === "judge") return q.options;
       return q.options.map(x => Number(x).toFixed(1));
     },
 
